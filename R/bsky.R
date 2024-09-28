dfbe9f08be4bb840080cc7a7ca1e98b247147e9e
ab604 <ab604@soton.ac.uk>
2024-09-28 16:26:01 +0100

    fix error

--- R/bsky.R
+++ R/bsky.R
@@ -5,14 +5,18 @@ library(dplyr)
 library(readr)
 library(lubridate)
 
+# Import data
 df <- read_csv("world-in-progress-articles-2024-09-28.csv")
 
+# Get this weeks article
 this_week <- df |> 
   arrange(post_week) |> 
   filter(post_week >= today() & post_week < today()+7)
 
+# Get the issue number of the article
 IN <- str_extract(this_week$issue_num,"(?<=issue-).*")
 
+# Path to article screenshot
 this_weeks_screenshot <- glue("img/{this_week$screen_shot_prefix}.png")
 
 # Create the post
